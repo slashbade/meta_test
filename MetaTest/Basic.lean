4f434e04e3ef8375f14d1251f55936a31036975f
slashbade <slashbladevitali@gmail.com>
2024-10-31 18:32:51 +0800

    augment proofs first test

--- MetaTest/Basic.lean
+++ MetaTest/Basic.lean
@@ -40,7 +40,7 @@ theorem gaussSum_mulShift2 (χ : MulChar R R') (ψ : AddChar R R') (a : Rˣ) :
   exact Fintype.sum_bijective _ a.mulLeft_bijective _ _ fun x ↦ rfl
 
 
-#check cube_of_not_dvd
+
 
 
 namespace Lean.Syntax
@@ -56,7 +56,288 @@ def setArgsr (stx: Syntax) (locs: List ℕ) (args: Array Syntax) : Syntax :=
   | .nil => stx.setArgs args
   | .cons i it => stx.setArg i $ stx[i].setArgsr it args
 
+
+partial def findall (stx: Syntax) (p: Syntax → Bool) : List Syntax :=
+  match stx with
+  | stx@(Syntax.node _ k args) =>
+    (if p stx then [stx] else []) ++ args.toList.bind (findall · p)
+  | _ => if p stx then [stx] else []
+
+
+def dedup_syntax : List Syntax → List Syntax :=
+  List.pwFilter (toString · ≠ toString ·)
+
+
+def getProofAfter (stx : Syntax) (pos: String.Pos) : Syntax := Id.run do
+  let tactic_seq := stx[1][3][1][1][0][0].getArgs
+  let tac_sep := tactic_seq[1]!
+  let mut after_seq : Array Syntax := #[]
+  for tac in tactic_seq do
+    let tac_pos := tac.getPos?.getD 0
+    if tac_pos > pos then
+      after_seq := after_seq.push tac
+  (stx.setArgsr [1,3,1,1,0,0] after_seq)[1][3]
+--   after_seq[1]!
+
 end Lean.Syntax
+
+
+namespace Lean.Elab.Info
+
+/-- The type of a `Lean.Elab.Info`, as a string. -/
+def kind : Info → String
+  | .ofTacticInfo         _ => "TacticInfo"
+  | .ofTermInfo           _ => "TermInfo"
+  | .ofCommandInfo        _ => "CommmandInfo"
+  | .ofMacroExpansionInfo _ => "MacroExpansionInfo"
+  | .ofOptionInfo         _ => "OptionInfo"
+  | .ofFieldInfo          _ => "FieldInfo"
+  | .ofCompletionInfo     _ => "CompletionInfo"
+  | .ofUserWidgetInfo     _ => "UserWidgetInfo"
+  | .ofCustomInfo         _ => "CustomInfo"
+  | .ofFVarAliasInfo      _ => "FVarAliasInfo"
+  | .ofFieldRedeclInfo    _ => "FieldRedeclInfo"
+  | .ofOmissionInfo       _ => "OmmisionInfo"
+
+/-- The `Syntax` for a `Lean.Elab.Info`, if there is one. -/
+def stx? : Info → Option Syntax
+  | .ofTacticInfo         info => info.stx
+  | .ofTermInfo           info => info.stx
+  | .ofCommandInfo        info => info.stx
+  | .ofMacroExpansionInfo info => info.stx
+  | .ofOptionInfo         info => info.stx
+  | .ofFieldInfo          info => info.stx
+  | .ofCompletionInfo     info => info.stx
+  | .ofUserWidgetInfo     info => info.stx
+  | .ofCustomInfo         info => info.stx
+  | .ofFVarAliasInfo      _    => none
+  | .ofFieldRedeclInfo    info => info.stx
+  | .ofOmissionInfo       info => info.stx
+
+/-- Is the `Syntax` for this `Lean.Elab.Info` original, or synthetic? -/
+def isOriginal (i : Info) : Bool :=
+  match i.stx? with
+  | none => true   -- Somewhat unclear what to do with `FVarAliasInfo`, so be conservative.
+  | some stx => match stx.getHeadInfo with
+    | .original .. => true
+    | _ => false
+
+end Lean.Elab.Info
+
+namespace Lean.Elab.ContextInfo
+
+/-- Pretty print an expression in the given `ContextInfo` with the given `LocalContext`. -/
+def ppExpr (ctx : ContextInfo) (lctx : LocalContext) (e : Expr) : IO Format :=
+  ctx.runMetaM lctx (do Meta.ppExpr (← instantiateMVars e))
+
+end Lean.Elab.ContextInfo
+
+namespace Lean.Elab.TacticInfo
+
+/-- Find the name for the outermost `Syntax` in this `TacticInfo`. -/
+def name? (t : TacticInfo) : Option Name :=
+  match t.stx with
+  | Syntax.node _ n _ => some n
+  | _ => none
+
+/-- Decide whether a tactic is "substantive",
+or is merely a tactic combinator (e.g. `by`, `;`, multiline tactics, parenthesized tactics). -/
+def isSubstantive (t : TacticInfo) : Bool :=
+  match t.name? with
+  | none => false
+  | some `null => false
+  | some ``cdot => false
+  | some ``cdotTk => false
+  | some ``Lean.Parser.Term.byTactic => false
+  | some ``Lean.Parser.Tactic.tacticSeq => false
+  | some ``Lean.Parser.Tactic.tacticSeq1Indented => false
+  | some ``Lean.Parser.Tactic.«tactic_<;>_» => false
+  | some ``Lean.Parser.Tactic.paren => false
+  | _ => true
+
+end Lean.Elab.TacticInfo
+
+namespace Lean.Elab.InfoTree
+
+-- /--
+-- Keep `.node` nodes and `.hole` nodes satisfying predicates.
+
+-- Returns a `List InfoTree`, although in most situations this will be a singleton.
+-- -/
+-- partial def filter (p : Info → Bool) (m : MVarId → Bool := fun _ => false) :
+--     InfoTree → List InfoTree
+--   | .context ctx tree => tree.filter p m |>.map (.context ctx)
+--   | .node info children =>
+--     if p info then
+--       [.node info (children.toList.map (filter p m)).join.toPArray']
+--     else
+--       (children.toList.map (filter p m)).join
+--   | .hole mvar => if m mvar then [.hole mvar] else []
+
+-- /-- Discard all nodes besides `.context` nodes and `TacticInfo` nodes. -/
+-- partial def retainTacticInfo (tree : InfoTree) : List InfoTree :=
+--   tree.filter fun | .ofTacticInfo _ => true | _ => false
+
+-- /-- Retain only nodes with "original" syntax. -/
+-- partial def retainOriginal (tree : InfoTree) : List InfoTree :=
+--   tree.filter Info.isOriginal
+
+-- /-- Discard all TacticInfo nodes that are tactic combinators or structuring tactics. -/
+-- -- There is considerable grey area here: what to do with `classical`?
+-- partial def retainSubstantive (tree : InfoTree) : List InfoTree :=
+--   tree.filter fun | .ofTacticInfo i => i.isSubstantive | _ => true
+
+/-- Discard any enclosing `InfoTree.context` layers. -/
+def consumeContext : InfoTree → InfoTree
+  | .context _ t => t.consumeContext
+  | t => t
+
+/-- Is this `InfoTree` a `TermInfo` for some `Expr`? -/
+def ofExpr? (i : InfoTree) : Option Expr := match i with
+  | .node (.ofTermInfo i) _ => some i.expr
+  | _ => none
+
+/-- Is this `InfoTree` a `TermInfo` for some `Name`? -/
+def ofName? (i : InfoTree) : Option Name := i.ofExpr?.bind Expr.constName?
+
+/-- Check if the `InfoTree` is the top level `InfoTree` for a declaration,
+if so, return it along with the declaration name. -/
+def elabDecl? (t : InfoTree) : Option Name :=
+  match t.consumeContext with
+  | .node (.ofCommandInfo i) c =>
+    if i.elaborator == `Lean.Elab.Command.elabDeclaration
+    then
+      -- this is hacky: we are relying on the ordering of the child nodes.
+      c.toList.foldr (fun cc acc => match (cc.consumeContext.ofName?, acc) with
+                       | (_, some r) => some r
+                       | (some n, none) => some n
+                       | (none, none) => none )
+                     none
+    else
+      none
+  | _ => none
+
+/-- Analogue of `Lean.Elab.InfoTree.findInfo?`, but that returns a list of all results. -/
+partial def findAllInfo (t : InfoTree) (ctx? : Option ContextInfo) (p : Info → Bool) :
+    List (Info × Option ContextInfo × PersistentArray InfoTree) :=
+  match t with
+  | context ctx t => t.findAllInfo (ctx.mergeIntoOuter? ctx?) p
+  | node i ts  =>
+      (if p i then [(i, ctx?, ts)] else []) ++ ts.toList.bind (fun t => t.findAllInfo ctx? p)
+  | _ => []
+
+/-- Return all `TacticInfo` nodes in an `InfoTree` corresponding to tactics,
+each equipped with its relevant `ContextInfo`, and any children info trees. -/
+def findTacticNodes (t : InfoTree) : List (TacticInfo × ContextInfo × PersistentArray InfoTree) :=
+  let infos := t.findAllInfo none fun i => match i with
+    | .ofTacticInfo _ => true
+    | _ => false
+  infos.filterMap fun p => match p with
+  | (.ofTacticInfo i, some ctx, children) => (i, ctx, children)
+  | _ => none
+
+
+def findTacticNodes' (t : InfoTree) : List (TacticInfo × ContextInfo × PersistentArray InfoTree) :=
+  let infos := t.findAllInfo none fun i => match i with
+    | .ofTacticInfo _ => true
+    | _ => false
+  let infos := infos.filterMap fun p => match p with
+  | (.ofTacticInfo i, some ctx, children) => (i, ctx, children)
+  | _ => none
+  let infos := infos.pwFilter fun (i1, c1, _) (i2, c2, _) =>
+    let l1 := c1.fileMap.toPosition (i1.toElabInfo.stx.getPos?.getD 0)
+    let l2 := c2.fileMap.toPosition (i2.toElabInfo.stx.getPos?.getD 0)
+    ¬l1.line == l2.line
+  infos
+
+end Lean.Elab.InfoTree
+
+def addLine (fmt : Format) : Format :=
+  if fmt.isNil then fmt else fmt ++ Format.line
+
+def Lean.Meta.ppGoalasDecl (mvarId: MVarId) : MetaM Format := do
+  match (← getMCtx).findDecl? mvarId with
+  | none          => return "unknown goal"
+  | some mvarDecl =>
+    let indent         := 2 -- Use option
+    let showLetValues  := pp.showLetValues.get (← getOptions)
+    let ppAuxDecls     := pp.auxDecls.get (← getOptions)
+    let ppImplDetailHyps := pp.implementationDetailHyps.get (← getOptions)
+    let lctx           := mvarDecl.lctx
+    let lctx           := lctx.sanitizeNames.run' { options := (← getOptions) }
+    withLCtx lctx mvarDecl.localInstances do
+      -- The following two `let rec`s are being used to control the generated code size.
+      -- Then should be remove after we rewrite the compiler in Lean
+      let rec pushPending (ids : List Name) (type? : Option Expr) (fmt : Format) : MetaM Format := do
+        if ids.isEmpty then
+          return fmt
+        else
+          let fmt := addLine fmt
+          match type? with
+          | none      => return fmt
+          | some type =>
+            let typeFmt ← ppExpr type
+            return fmt ++ ("(" ++ Format.joinSep ids.reverse (format " ") ++ " :" ++ Format.nest indent (Format.line ++ typeFmt) ++ ")").group
+      let rec ppVars (varNames : List Name) (prevType? : Option Expr) (fmt : Format) (localDecl : LocalDecl) : MetaM (List Name × Option Expr × Format) := do
+        match localDecl with
+        | .cdecl _ _ varName type _ _ =>
+          let varName := varName.simpMacroScopes
+          let type ← instantiateMVars type
+          if prevType? == none || prevType? == some type then
+            return (varName :: varNames, some type, fmt)
+          else do
+            let fmt ← pushPending varNames prevType? fmt
+            return ([varName], some type, fmt)
+        | .ldecl _ _ varName type val _ _ => do
+          let varName := varName.simpMacroScopes
+          let fmt ← pushPending varNames prevType? fmt
+          let fmt  := addLine fmt
+          let type ← instantiateMVars type
+          let typeFmt ← ppExpr type
+          let mut fmtElem  := format varName ++ " : " ++ typeFmt
+          if showLetValues then
+            let val ← instantiateMVars val
+            let valFmt ← ppExpr val
+            fmtElem := fmtElem ++ " :=" ++ Format.nest indent (Format.line ++ valFmt)
+          let fmt := fmt ++ fmtElem.group
+          return ([], none, fmt)
+      let (varNames, type?, fmt) ← lctx.foldlM (init := ([], none, Format.nil)) fun (varNames, prevType?, fmt) (localDecl : LocalDecl) =>
+         if !ppAuxDecls && localDecl.isAuxDecl || !ppImplDetailHyps && localDecl.isImplementationDetail then
+           return (varNames, prevType?, fmt)
+         else
+           ppVars varNames prevType? fmt localDecl
+    --   return fmt
+      let fmt ← pushPending varNames type? fmt
+    --   return fmt
+      let fmt := addLine fmt
+      let typeFmt ← ppExpr (← instantiateMVars mvarDecl.type)
+      let fmt := fmt ++ ": " ++ Format.nest indent typeFmt
+      return "lemma aug " ++ fmt
+    --   match mvarDecl.userName with
+    --   | Name.anonymous => return fmt
+    --   | name           => return "case " ++ format name.eraseMacroScopes ++ Format.line ++ fmt
+
+
+def Lean.Elab.ContextInfo.ppGoals_as_decl (ctx : ContextInfo) (goals : List MVarId) : IO Format :=
+    if goals.isEmpty then
+      return "no goals"
+    else
+        ctx.runMetaM {} (return Std.Format.prefixJoin "\n" (← goals.mapM (Meta.ppGoalasDecl ·)))
+
+
+
+
+
+
+
+
+
+namespace Augmenter
+
+
+end Augmenter
+
+
 
 open Lean.Syntax Lean.Parser Lean.Elab
 
@@ -84,11 +365,23 @@ elab "#info " c:command : command => do
   let initInfoTrees ← getResetInfoTrees
   try
     elabCommand c
+    let stxs := c.raw.findall (·.isOfKind identKind)
+    -- logInfo m!"{c.raw[1][3][1][1][0][0][0][1].getKind}"
+    logInfo m!"Syntax references: {dedup_syntax stxs}"
     let trees ← getInfoTrees
     for tree in trees do
-      let fm := tree.getCompletionInfos
-      -- logInfo m!"{fm}"
-      logInfo m!"{← tree.format}"
+      let fm := tree.findTacticNodes'
+      for (info, ctx, children) in fm do
+        let ctxB := { ctx with mctx := info.mctxBefore }
+        let ctxA := { ctx with mctx := info.mctxAfter }
+        let goalsBefore ← ctxB.ppGoals info.goalsBefore
+        let goalsAfter  ← ctxA.ppGoals_as_decl info.goalsAfter
+        let goalsPos := info.toElabInfo.stx.getPos?.getD 0
+        let proofAfter := c.raw.getProofAfter goalsPos
+        logInfo m!"Augmented Proof No.{goalsPos}\n{goalsAfter}{proofAfter}"
+        -- logInfo m!"{proofAfter}"
+        -- logInfo m!"{}"
+    --   logInfo m!"{← tree.format}"
   finally
     modify fun st =>
       { st with infoState := { st.infoState with trees := initInfoTrees ++ st.infoState.trees } }
@@ -115,3 +408,6 @@ lemma cube_o2f_not_dvd1 {n : ℤ} (h : ¬3 ∣ n) : (n : ZMod 9) ^ 3 = 1 ∨ (n 
   rw [Ne]
   rw [ZMod.intCast_zmod_eq_zero_iff_dvd]
   assumption
+
+
+example (a n : ℤ) (h : ¬3 ∣ n) : ↑n ≠ 0 := sorry
